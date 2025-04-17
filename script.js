5d7cf60bc1d15122644b2a03c47731680c52f58d
bra <bra@test.com>
2025-04-17 22:42:57 +0200

    creati moduli per gestione fetch

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-async function getArtists(){
-    let response = await fetch("http://localhost:4000/api/artists/");
-    let artists = await response.json();
-    console.log(artists)
-}
-
-async function getArtist(artistId){
-    try {
-        let response = await fetch(`http://localhost:4000/api/artists/${artistId}`);
-        if(response.status === 404){
-            throw new Error("band not found")
-        }
-        let artist = await response.json();
-        console.log(artist)
-    } catch (error){
-        console.log(error)
-    }
-}
+import { getArtists, getArtist } from "./modules/artistApi";
 
 getArtists();
 getArtist("6643b1688c94709aaaa9db7f");
