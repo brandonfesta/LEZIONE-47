1b918728104d7d5e0850cbf0099955f944784d33
bra <bra@test.com>
2025-04-17 22:29:51 +0200

    fetch request per tutti artisti e singolo artista in js

---
+++ script.js
@@ -0,0 +1,21 @@
+async function getArtists(){
+    let response = await fetch("http://localhost:4000/api/artists/");
+    let artists = await response.json();
+    console.log(artists)
+}
+
+async function getArtist(artistId){
+    try {
+        let response = await fetch(`http://localhost:4000/api/artists/${artistId}`);
+        if(response.status === 404){
+            throw new Error("band not found")
+        }
+        let artist = await response.json();
+        console.log(artist)
+    } catch {
+        console.log(error)
+    }
+}
+
+getArtists();
+getArtist("6643b1688c94709aaaa9db7f");
