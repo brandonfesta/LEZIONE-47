a38eb603d0fed6212a1f2585c652f0674abbf566
bra <bra@test.com>
2025-04-18 08:31:51 +0200

    crazione di card primordiali con solo titolo per esempio

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 import { getArtists, getArtist, deleteArtist } from "./modules/artistApi.js";
 
+let artistsContainer = document.getElementById("artists-container");
+
 async function createArtistCards(){
     let artists = await getArtists();
+
+    artists.map(artist => {
+        let artistContainer = document.createElement("article");
+        let artistTitle = document.createElement("h2");
+        artistTitle.textContent = artist.name;
+
+        artistContainer.append(artistTitle);
+        artistsContainer.append(artistContainer)
+    })
 }
 
 createArtistCards();
