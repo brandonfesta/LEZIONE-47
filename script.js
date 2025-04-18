130c9c98e7576aa107d3fd89c23b7b4e9a8eec45
bra <bra@test.com>
2025-04-18 08:59:35 +0200

    aggiunte pure immagini

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ async function createArtistCards(){
 
     artists.map(artist => {
         let artistContainer = document.createElement("article");
+        let artistImage = document.createElement("img")
         let artistTitle = document.createElement("h2");
         artistTitle.textContent = artist.name;
+        artistImage.src = "http://localhost:4000/" + artist.image;
 
+        artistContainer.append(artistImage);
         artistContainer.append(artistTitle);
-        artistsContainer.append(artistContainer)
+        artistsContainer.append(artistContainer);
     })
 }
 
